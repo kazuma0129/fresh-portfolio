093f0fdad91b3768cba95815a4f0fd6f98cd2e4d
KazumaOhashi <tikuwaaaaaaaa@gmail.com>
2022-07-05 00:37:13 +0900

    Add /posts

---
+++ routes/posts/index.tsx
@@ -0,0 +1,8 @@
+/** @jsx h */
+import { h } from 'preact';
+import { tw } from '@twind';
+import { frontMatter, gfm } from '../../utils/markdown.ts';
+
+export default () => {
+  return <div>Now implementing....</div>;
+};
