c4273f96c7cc690ce510aa34c5929fc01c128f07
KazumaOhashi <kazuma0129.work@gmail.com>
2023-03-08 21:18:27 +0900

    Fix spacing

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,18 @@
 /** @jsx h */
-import { h } from 'preact';
-import { tw } from '@twind';
-import { Card } from '../components/Card.tsx';
-import { ExternalLink } from '../components/ExternalLink.tsx';
-import { writings, qualifications, careers, features, links } from '../constants/about.ts';
-import LastUpdated from '../islands/LastUpdated.tsx';
-import ThemeButton from '../islands/ThemeButton.tsx';
-import { FlexLinkAndDateRow } from '../components/FlexRow.tsx';
+import { h } from "preact";
+import { tw } from "@twind";
+import { Card } from "../components/Card.tsx";
+import { ExternalLink } from "../components/ExternalLink.tsx";
+import {
+  writings,
+  qualifications,
+  careers,
+  features,
+  links,
+} from "../constants/about.ts";
+import LastUpdated from "../islands/LastUpdated.tsx";
+import ThemeButton from "../islands/ThemeButton.tsx";
+import { FlexLinkAndDateRow } from "../components/FlexRow.tsx";
 
 export default function Portfolio() {
   const cardBodyStyle = tw`p-2 md:p-4 font-semibold`;
@@ -17,21 +23,26 @@ export default function Portfolio() {
         <h2 class={tw`text-lg`}>Software Engineer, Tokyo, Japan</h2>
       </div>
       <div class={tw`flex flex-row flex-wrap mb-20`}>
-        <Card title='Background'>
+        <Card title="Background">
           <div class={tw`${cardBodyStyle}`}>
             <p>I was born and raised in Aomori Prefecture, Japan.</p>
             <p>
-              Graduate of
-              <ExternalLink url='https://www.fun.ac.jp/en' title='Future University Hakodate.' />
+              Graduate of{" "}
+              <ExternalLink
+                url="https://www.fun.ac.jp/en"
+                title="Future University Hakodate."
+              />{" "}
               Bachelor of System Information Science.
             </p>
             <p>I'm currently working as a software Engineer in Tokyo, Japan.</p>
           </div>
         </Card>
-        <Card title='Interests'>
-          <div class={tw`${cardBodyStyle}`}>Motorcycle, Netflix, Comedy, Ramen, ONSEN</div>
+        <Card title="Interests">
+          <div class={tw`${cardBodyStyle}`}>
+            Motorcycle, Netflix, Comedy, Ramen, ONSEN
+          </div>
         </Card>
-        <Card title='Writings'>
+        <Card title="Writings">
           <div class={tw`${cardBodyStyle}`}>
             {writings.map((item) => {
               return (
@@ -42,24 +53,42 @@ export default function Portfolio() {
             })}
           </div>
         </Card>
-        <Card title='Qualifications'>
+        <Card title="Qualifications">
           <div class={tw`${cardBodyStyle}`}>
             {qualifications.map((item) => {
-              return <FlexLinkAndDateRow date={item.date} url={item.url} title={item.name} />;
+              return (
+                <FlexLinkAndDateRow
+                  date={item.date}
+                  url={item.url}
+                  title={item.name}
+                />
+              );
             })}
           </div>
         </Card>
-        <Card title='Carrers'>
+        <Card title="Carrers">
           <div class={tw`${cardBodyStyle}`}>
             {careers.map((item) => {
-              return <FlexLinkAndDateRow date={item.date} url={item.url} title={item.corpName} />;
+              return (
+                <FlexLinkAndDateRow
+                  date={item.date}
+                  url={item.url}
+                  title={item.corpName}
+                />
+              );
             })}
           </div>
         </Card>
-        <Card title='Features'>
+        <Card title="Features">
           <div class={tw`${cardBodyStyle}`}>
             {features.map((item) => {
-              return <FlexLinkAndDateRow date={item.date} url={item.url} title={item.title} />;
+              return (
+                <FlexLinkAndDateRow
+                  date={item.date}
+                  url={item.url}
+                  title={item.title}
+                />
+              );
             })}
           </div>
         </Card>
